d0a54954248870da156d9dd5b139225f987f0932
孙颖洲 <sunyz94123@qq.com>
2024-11-19 21:17:56 +0800

    feat: 更新ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const { addIconSelectors } = require('@iconify/tailwind')
+
 module.exports = {
   content: ['./dist/**/*.{html,js}'],
   theme: {
-    extend: {}
-  },
-  plugins: []
+    extend: {
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  }
+  // plugins: [addIconSelectors(['feather', 'heroicons'])]
 }
